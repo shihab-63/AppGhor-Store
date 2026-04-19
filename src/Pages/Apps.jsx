beeb58b71bddfa46605e4b67365cc3bd7b089d7d
shihab-63 <thecodzhub@gmail.com>
2026-04-19 11:07:45 +0600

    add search functionality

--- src/Pages/Apps.jsx
+++ src/Pages/Apps.jsx
@@ -1,12 +1,27 @@
-import React from "react";
-import useAppsData from "../Hooks/useAppsData";
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import { useState } from "react";
+import { ScrollRestoration } from "react-router";
 import Container from "../Components/Container/Container";
 import App from "../Components/TrandingApps/App";
-import { ScrollRestoration } from "react-router";
+import useAppsData from "../Hooks/useAppsData";
 
 const Apps = () => {
   const { apps } = useAppsData();
+  const [search, setSearch] = useState("");
+  const [isSearching, setIsSearching] = useState(false);
+  const filteredApps = apps.filter((app) =>
+    app.title.toLowerCase().includes(search.toLowerCase()),
+  );
+
+  const handleChange = (e) => {
+    const searchOnly = e.target.value;
+    setIsSearching(true);
+    setTimeout(() => {
+      setSearch(searchOnly);
+      setIsSearching(false);
+    }, 500);
+  };
+
   return (
     <div className="py-12">
       <ScrollRestoration />
@@ -21,7 +36,9 @@ const Apps = () => {
         </div>
 
         <div className="flex items-center justify-between mt-12">
-          <p className="text-base md:text-md">({apps.length}) Apps Found</p>
+          <p className="text-base md:text-md">
+            ({filteredApps.length}) Apps Found
+          </p>
           <label className="input w-44 md:w-fit focus-within:outline-none focus-within:border-violet-500 border-gray-300">
             <svg
               className="h-[1em] md:h-[1.5em] opacity-50"
@@ -40,6 +57,8 @@ const Apps = () => {
               </g>
             </svg>
             <input
+              value={search}
+              onChange={handleChange}
               type="search"
               className="grow bg-transparent border-none outline-none focus:outline-none focus:ring-0"
               placeholder="Search your apps"
@@ -51,9 +70,33 @@ const Apps = () => {
           <div className="w-full max-w-3xl md:max-w-5xl mx-auto">
             <DotLottieReact src="/loading.lottie" loop autoplay />
           </div>
+        ) : filteredApps.length === 0 ? (
+          <div className="text-center py-15">
+            <DotLottieReact
+              className="md:w-96 mx-auto"
+              loop
+              autoplay
+              src="/NoItemFound.lottie"
+            />
+            <p className="text-lg font-medium pt-4">
+              Oops! No apps found for "{search}"
+            </p>
+            <span className="text-gray-400 font-light">
+              Try searching with a different keyword.⚡
+            </span>
+          </div>
+        ) : isSearching ? (
+          <div className="text-center py-15">
+            <DotLottieReact
+              className="md:w-96 mx-auto"
+              loop
+              autoplay
+              src="/Search.lottie"
+            />
+          </div>
         ) : (
           <div className="grid grid-cols-2 md:grid-cols-4 gap-3 md:gap-5 px-3 md:px-0 py-8">
-            {apps.map((app) => (
+            {filteredApps.map((app) => (
               <App key={app.id} app={app} />
             ))}
           </div>
